24b643964e16b76d93d1c1ffdd8b1fbb3c358608
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-01-09 20:36:20 +1300

    update about and contact pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,8 @@ import Header from "../components/header"
 export default function Contact() {
     return (
         <div style={{ color: `teal` }}>
-            <Link to="/">Home</Link>
+            <Link to="/">Home </Link>
+            <Link to="/contact/"> Contact</Link>
             <Header headerText="Contact" />
             <p>Send me a message at </p><br/>
             <p>rodrigohdiaz@gmail.com</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,10 @@ import Header from "../components/header"
 export default function Home() {
   return (
   <div style={{ color: 'purple' }}>
-    <Link to="/contact/">Contact</Link>
+    <Link to="/about/">About </Link>
+    <Link to="/contact/"> Contact</Link>
     <Header headerText="Rodrigo Diaz" />
-    <p>This is a little blog, so come and hang out for a little while</p>
+    <p>Come on, sit down and hang out for a while.</p>
     <img src="../../images/toronto5.jpg" alt="toronto" style={{ height: "700px", display: "block", margin: "0 auto" }} />
   </div>
   )
