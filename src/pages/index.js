7a833038553e65668d6f52e50364ab1f34877f3c
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-01-18 21:52:48 +1300

    create nested layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,22 @@ import Header from "../components/header"
 
 export default function Home() {
   return (
-  <div style={{ color: 'purple' }}>
+  <div style={{ color: 'purple', margin: `3rem auto`, maxWidth: 600 }}>
     <Link to="/about/">About </Link>
     <Link to="/contact/"> Contact</Link>
     <Header headerText="Rodrigo Diaz" />
     <p>Come on, sit down and hang out for a while.</p>
     <img src="../../images/toronto5.jpg" alt="toronto" style={{ height: "700px", display: "block", margin: "0 auto" }} />
+  
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
   </div>
+  
   )
 }
+
+
+
