4dd0ff6d9f1f906904730d206cadee9cc28050e5
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-02-03 20:19:08 +1300

    adds excercise about metaData

---
+++ tutorial-part-four/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pandas eating a lot!`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
