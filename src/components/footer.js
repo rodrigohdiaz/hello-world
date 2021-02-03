93f4f4e2a834d60495c8fc1d65a50164623256a5
rodrigo-diaz <rodrigohdiaz@gmail.com>
2021-02-03 19:08:20 +1300

    modiefies footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const ListLink = props => (
-  <li style={{ display: `inline-block`, marginBottom: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
 
 export default function Footer ({ children }) {
     return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,6 @@ export default function Home() {
       <Header headerText="Rodrigo Diaz" />
       <p>Come on, sit down and hang out for a while.</p>
       <img src="../../images/toronto5.jpg" alt="toronto" style={{ height: "700px", display: "block", margin: "0 auto" }} />
-    
-      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-      <p>
-        What do I like to do? Lots of course but definitely enjoy building
-        websites.
-      </p>
     </div>
     <Footer></Footer>
   </Layout>
